c5c62a2bbb4061c16bbcdb9858ecb40fd4a4e76f
Yuichiro Iwashita <9864901+yiRMT@users.noreply.github.com>
2023-03-29 17:17:26 +0900

    ChatGPT APIにリクエストできるようにした (#2)

--- src/chatGPT/askGPT.js
+++ src/chatGPT/askGPT.js
@@ -1,20 +1,17 @@
 const { Configuration, OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-    apiKey: process.env.REACT_APP_OPENAI_KEY,
-});
-const openai = new OpenAIApi(configuration);
+export const askGPT = async (apikey, content, model = "gpt-3.5-turbo-0301") => {
+    const configuration = new Configuration({
+        apiKey: apikey,
+    });
 
+    const openai = new OpenAIApi(configuration);
 
-
-export async function ask(content, model = "gpt-3.5-turbo-0301") {
     const response = await openai.createChatCompletion({
         model: model,
         messages: [{ role: "user", content: content }],
     });
 
     const answer = response.data.choices[0].message?.content;
-    console.log(answer);
+    return answer
 }
-
-
